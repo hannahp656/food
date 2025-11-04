42ae0ea93b4b17e26a1700ea259b494bea6c0828
hannahp656 <hannahp656@gmail.com>
2025-11-03 18:27:51 -0600

    Add files via upload

--- recipe-files.js
+++ recipe-files.js
@@ -2,12 +2,23 @@
 const recipeFiles = [
   "/food/recipes/recipe-baby-carrots.html",
   "/food/recipes/recipe-breakfast-potatoes.html",
+  "/food/recipes/recipe-breakfast-tacos.html",
   "/food/recipes/recipe-cereal.html",
+  "/food/recipes/recipe-chicken-quesadillas.html",
   "/food/recipes/recipe-crunchy-beef-tacos.html",
+  "/food/recipes/recipe-cucumber-and-lime-slices.html",
+  "/food/recipes/recipe-egg-and-cheese-croissant.html",
+  "/food/recipes/recipe-egg-and-cheese-omlette.html",
+  "/food/recipes/recipe-egg-and-cheese-sandwich.html",
   "/food/recipes/recipe-egg-whites.html",
   "/food/recipes/recipe-gogo-squeez.html",
   "/food/recipes/recipe-granny-smith-apple.html",
+  "/food/recipes/recipe-grilled-ham-and-cheese.html",
+  "/food/recipes/recipe-hot-dog.html",
   "/food/recipes/recipe-moms-meatballs.html",
+  "/food/recipes/recipe-oatmeal.html",
   "/food/recipes/recipe-peanut-butter-waffle.html",
-  "/food/recipes/recipe-shanghai-scallion-oil-noodles-and-pork.html"
+  "/food/recipes/recipe-shanghai-scallion-oil-noodles-and-pork.html",
+  "/food/recipes/recipe-sub-sandwich.html",
+  "/food/recipes/recipe-tuna-salad-sandwich.html"
 ];
