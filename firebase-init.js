f073afbf8b8205448412b8d18977119460aa4f52
hannahp656 <hannahp656@gmail.com>
2025-11-30 18:24:51 -0600

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,20 +1,21 @@
+// firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } 
-  from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore } 
-  from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// YOUR Firebase config goes here ↓↓↓
-const firebaseConfig = {
-  apiKey: "YOUR_KEY_HERE",
-  authDomain: "YOUR_DOMAIN.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "123",
-  appId: "YOUR_APP_ID"
+export const firebaseConfig = {
+  apiKey: "AIzaSyAvKuWusC_jMg23pB3PGSspHA7qAotb4Hc",
+  authDomain: "recipes-372f8.firebaseapp.com",
+  projectId: "recipes-372f8",
+  storageBucket: "recipes-372f8.firebasestorage.app",
+  messagingSenderId: "874512404094",
+  appId: "1:874512404094:web:80e7143fbe47417d27d22f"
 };
 
+// Initialize
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
+// Google provider
+export const provider = new GoogleAuthProvider();
